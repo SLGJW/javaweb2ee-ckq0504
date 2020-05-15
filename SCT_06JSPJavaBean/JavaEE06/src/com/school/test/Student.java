18ca1d3faa076a6a0d7e6124701b9475f01dad19
ckq0504 <62922038+ckq0504@users.noreply.github.com>
2020-05-15 10:26:45 +0800

    1718064059
    1718064059

---
+++ SCT_06JSPJavaBean/JavaEE06/src/com/school/test/Student.java
@@ -0,0 +1,49 @@
+package com.school.test;
+
+import java.sql.Date;
+
+public class Student {
+    private String sid;
+    private String sname;
+    private String ssex;
+
+    public Student(){}
+    public Student(String sid, String sname, String ssex){
+        this.sid=sid;
+        this.sname=sname;
+        this.ssex=ssex;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "sid='" + sid + '\'' +
+                ", sname='" + sname + '\'' +
+                ", ssex='" + ssex + '\'' +
+                '}';
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getSsex() {
+        return ssex;
+    }
+
+    public void setSsex(String ssex) {
+        this.ssex = ssex;
+    }
+}
